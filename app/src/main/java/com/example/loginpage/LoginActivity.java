75771d980521792befc8e57107dee2f3f26dd072
AzeezFazry <azeezfazry@gmail.com>
2021-06-05 23:02:48 +0530

    Email and password verification codes included.

--- app/src/main/java/com/example/loginpage/LoginActivity.java
+++ app/src/main/java/com/example/loginpage/LoginActivity.java
@@ -7,6 +7,13 @@ package com.example.loginpage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,4 +22,45 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void buSignupAction(View view) {
+
+        // declaration of components
+        EditText fullNameET = (EditText) findViewById(R.id.etFirstName);
+        EditText usernameET = (EditText) findViewById(R.id.etUsername);
+        EditText passwordET = (EditText) findViewById(R.id.etPassword);
+        EditText confirmPasswordET = (EditText) findViewById(R.id.etConfirmPassword);
+
+        String fullname = fullNameET.getText().toString();
+        String username = usernameET.getText().toString();
+        String password = passwordET.getText().toString();
+        String confirmPassword = confirmPasswordET.getText().toString();
+
+        if(!(Patterns.EMAIL_ADDRESS.matcher(username).matches())){
+            Toast.makeText(LoginActivity.this, "Invalid E-mail address", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (!(password.length() > 7 && isValidPassword(password,confirmPassword))) {
+            Toast.makeText(LoginActivity.this, "Incorrect Password", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // TODO link the home page
+    }
+
+    /**
+     * Function to validate the password
+     * @param password
+     * @param confirmPassword
+     * @return boolean
+     */
+    private boolean isValidPassword(String password, String confirmPassword){
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        if(matcher.matches()) return true;
+        else return false;
+    }
+
 }
--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
     public void buSignupActivity(View view) {
         Intent intent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void buLoginAction(View view) {
+        EditText usernameET = findViewById(R.id.etEmail);
+        String username = usernameET.getText().toString();
+
+        EditText passwordET = findViewById(R.id.etPassword);
+        String password = passwordET.getText().toString();
+
+        Toast.makeText(MainActivity.this, "Completed", Toast.LENGTH_LONG).show();
+
     }
 }
